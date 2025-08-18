90e27661b5a4ffb9ccc9ff66aa5ecaa3c2200de9
Dennis Olsen <dennan_lol@hotmail.com>
2025-08-18 11:10:59 +0200

    Filtering and categories implemented

--- src/components/FastFoodMenu.jsx
+++ src/components/FastFoodMenu.jsx
@@ -4,15 +4,17 @@ import Cart from "./Cart.jsx";
 
 const FastFoodMenu = () => {
     const foodItems = [
-        { id: 1, title: "Burger", description: "A delicious buger with all the fixings.", price: 5.99, image: "https://placehold.co/150"},
-        { id: 2, title: "Pizza", description: "Classic pizza with your favorite toppings.", price: 7.99, image: "https://placehold.co/150"},
-        { id: 3, title: "Fries", description: "Crispy golden fries served hot and fresh.", price: 2.99, image: "https://placehold.co/150"}
+        { id: 1, title: "Burger", description: "A delicious buger with all the fixings.", price: 5.99, image: "https://placehold.co/150", catergories: ["All", "Main"] },
+        { id: 2, title: "Pizza", description: "Classic pizza with your favorite toppings.", price: 7.99, image: "https://placehold.co/150", catergories: ["All","Main"]},
+        { id: 3, title: "Fries", description: "Crispy golden fries served hot and fresh.", price: 2.99, image: "https://placehold.co/150", catergories: ["All", "Secondary"] }
     ];
     const cartItems = [
         { id: 1, title: "Burger", count: 0, price: 5.99 },
         { id: 2, title: "Pizza", count: 0, price: 7.99},
         { id: 3, title: "Fries", count: 0, price: 2.99}
     ];
+    const [selectedCategory, setSelectedCategory] = useState("All");
+    const foodCategories = ["All", "Main", "Secondary"];
     const [cart, setCart] = useState(cartItems);
 
     // Used to change between dark and light mode
@@ -66,16 +68,29 @@ const FastFoodMenu = () => {
         setTextAndBtnColor(textAndBtnColor === "dark" ? "light" : "dark");
     };
 
+    const changeFilter = (category) => {
+        setSelectedCategory(category);
+    };
+
+
     // The most outer div is the container for the entire menu so the background color and text color are applied here.
     // Only the FastFoodCard needs background color and text color props to apply the styles to each card.
     return (
         <div className={`bg-${backgroundColor} text-${textAndBtnColor} p-3`}>
             <header className={`p-3`}>
                 <button className={`btn btn-${textAndBtnColor}`} onClick={() => changeDarkMode()}>Dark</button>
+
+                {foodCategories.map(category => (
+                    <button className={`btn btn-${textAndBtnColor} ms-2 `} onClick={() => changeFilter(category)}>{category}</button>
+                ))};
+                
                 <h1 className="text-center">Fast Food Menu</h1>
-                <div className="container">
+
+                
+            </header>
+            <div className="container">
                     <div className="row">
-                        {foodItems && foodItems.map(item => (
+                        {foodItems && foodItems.map(item => (item.catergories.find(category => category === selectedCategory) === selectedCategory) && (
                             <FastFoodCard 
                             key={item.id}
                             title={item.title}
@@ -89,7 +104,7 @@ const FastFoodMenu = () => {
                         ))}
                     </div>
                 </div>
-            </header>
+
             <Cart
                 items={cart}
                 backgroundColor={backgroundColor}
